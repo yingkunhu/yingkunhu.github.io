6f5bdfaef7eb799850f56733b814e1eacf20eaf4
Yingkun Hu <I025366@PVGL50819909B.apj.global.corp.sap>
2015-06-12 18:16:13 +0800

    change to unix port, and use JSON for transmission

--- nodejs-samples/net-watcher.js
+++ nodejs-samples/net-watcher.js
@@ -18,9 +18,18 @@ require('fs').exists(fileName, function(exists) {
 function createServer(p_file) {
 	const server=net.createServer(function(connection) {
 		console.log('Subscriber connected!');
-		connection.write("Now watching '" + p_file + "' for changes\n");
+		var objWatching={};
+		objWatching.type='watching';
+		objWatching.file=p_file;
+		//connection.write("Now watching '" + p_file + "' for changes\n");
+		connection.write(JSON.stringify(objWatching) +'\n');
 		let watcher=require('fs').watch(p_file, function() {
-			connection.write("File '" + p_file + "' changed at " + Date.now().toString() + "\n");
+			//connection.write("File '" + p_file + "' changed at " + Date.now().toString() + "\n");
+			var obj={};
+			obj.type='changed';
+			obj.file=p_file;
+			obj.timestamp=Date.now();
+			connection.write(JSON.stringify(obj) +'\n');
 		});
 		connection.on("data", function(data) {
 			console.log("There are data received from connection:"  + data.toString());
@@ -31,7 +40,10 @@ function createServer(p_file) {
 		});
 	});
 
-	server.listen(5432, function() {
+	/* server.listen(5432, function() {
 		console.log("Server started and listening on port 5432.");	
+	});*/
+	server.listen('/tmp/watcher.sock', function() {
+		console.log('Listenning for subscribers on unix socket /tmp/watcher.sock');
 	});
 }
