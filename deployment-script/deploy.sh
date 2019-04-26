29168b524ddda3cb949df2e57b2b6722989a124e
Yingkun Hu <y.hu@sap.com>
2019-04-26 20:34:44 +1200

    test

---
+++ deployment-script/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Checking OS type ..."
+if [[ ! -f /etc/centos-release ]]; then
+  1>&2 echo Not CentOS Linux
+  exit 1
+fi
+
+echo "Checking OS Version ..."
+if ! grep -lq "CentOS Linux release 7\\.[0-9]" /etc/centos-release; then
+  1>&2 echo Only CentOS Linux 7.x is supported.
+  exit 1
+fi
+
+echo "Checking current user ..."
+if [[ ! $(id -u) == 0 ]]; then
+  1>&2 echo only root user can deploy.
+  exit 1
+fi
+
